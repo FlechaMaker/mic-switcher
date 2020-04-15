8f0fe86122cb8241e49b28db4d21bf6db84da927
FlechaMaker <flechacreador@gmail.com>
2020-04-15 11:28:29 +0900

    Notify when volume is changed

--- micsw.py
+++ micsw.py
@@ -8,15 +8,17 @@ if __name__ == "__main__":
     print(sys.argv[1])
     ser = serial.Serial(sys.argv[1], 115200)
     prev = int(ser.read()[0])
-    subprocess.call(['osascript', '-e', 'set volume output volume 0'])
+    subprocess.call(['osascript', '-e', 'set volume input volume 0'])
     while True:
         sw = int(ser.read()[0])
         if sw != prev:
             if sw == 0:
                 subprocess.call(['osascript', '-e', 'set volume input volume 100'])
+                subprocess.call(['terminal-notifier', '-message', 'Input ON'])
                 print(100)
             else:
                 subprocess.call(['osascript', '-e', 'set volume input volume 0'])
+                subprocess.call(['terminal-notifier', '-message', 'Input OFF'])
                 print(0)
         prev = sw
 
